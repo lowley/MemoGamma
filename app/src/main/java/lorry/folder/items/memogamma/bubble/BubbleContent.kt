2255595f41cd33c8423eabbaf8b535f2874664b7
olivier SAINT-EVE <osainteve@gmail.com>
2025-04-23 13:09:24 +0200

    wip amélioration ergo bubble

--- app/src/main/java/lorry/folder/items/memogamma/bubble/BubbleContent.kt
+++ app/src/main/java/lorry/folder/items/memogamma/bubble/BubbleContent.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ThumbDown
 import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -17,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -30,38 +33,48 @@ fun BubbleContent(viewModel: BubbleViewModel) {
         BubbleState.HIDDEN
     )
 
-    if (visibilityState != BubbleState.HIDDEN) {
-        Surface(
+    Surface(
+        modifier = Modifier
+            //.align(Alignment.Center)
+            .wrapContentSize()
+            .background(Color.Transparent),
+        shape = MaterialTheme.shapes.medium,
+        color = Color.Transparent,
+        tonalElevation = 0.dp,
+        border = BorderStroke(1.dp, Color.DarkGray),
+    ) {
+        Column(
             modifier = Modifier
-                //.align(Alignment.Center)
-                .wrapContentSize()
-                .background(Color.Transparent),
-            shape = MaterialTheme.shapes.medium,
-            color = Color.Transparent,
-            tonalElevation = 0.dp,
-            border = BorderStroke(1.dp, Color.DarkGray),
+                .background(Color.Black.copy(alpha = 0.2f))
+                .padding(0.dp)
+                .zIndex(1f),
+            verticalArrangement = Arrangement.Center
         ) {
-            FloatingActionButton(
-                modifier = Modifier
-                    .dragFloatingWindow()
-                    .zIndex(0f), // au-dessus
-                onClick = {
-                    BubbleManager.toggleBubbleTotal()
-                }
-            ) {
-                Icon(modifier = Modifier
-                    .zIndex(0f),
-                    imageVector = Icons.Default.ThumbUp, contentDescription = "Ouvrir / Fermer")
-            }
-            if (visibilityState == BubbleState.TOTAL) {
-                Column(
+            if (visibilityState != BubbleState.HIDDEN) {
+                FloatingActionButton(
                     modifier = Modifier
-                        .background(Color.Black.copy(alpha = 0.2f))
-                        .padding(16.dp)
-                        .zIndex(1f),
-                    verticalArrangement = Arrangement.Center
+                        .align(Alignment.Start)
+                        .dragFloatingWindow()
+                        .zIndex(0f)
+                        .size(40.dp), // au-dessus
+                    onClick = {
+                        BubbleManager.toggleBubbleTotal()
+                    }
                 ) {
-                    Text("inside the bubble")
+                    Icon(
+                        modifier = Modifier
+                            .zIndex(0f),
+                        imageVector = if (visibilityState == BubbleState.BUBBLE)
+                            Icons.Default.ThumbUp else Icons.Default.ThumbDown,
+                        contentDescription = "Ouvrir / Fermer"
+                    )
+                }
+
+                if (visibilityState == BubbleState.TOTAL) {
+                    Text(
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        text = "inside the bubble"
+                    )
 
                 }
             }
