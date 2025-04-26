42e693fdf064398ad94d3f513cf8370b3498fa15
olivier SAINT-EVE <osainteve@gmail.com>
2025-04-26 15:50:07 +0200

    mets en place le scroll à 2 doigts

--- app/src/main/java/lorry/folder/items/memogamma/bubble/BubbleContent.kt
+++ app/src/main/java/lorry/folder/items/memogamma/bubble/BubbleContent.kt
@@ -48,14 +48,18 @@ fun BubbleContent(viewModel: BubbleViewModel) {
     //val floatingWindow = LocalFloatingWindow.current
     val visibilityState by viewModel.bubbleState.collectAsState(BubbleState.HIDDEN)
     val stylusColor by viewModel.stylusColor.collectAsState()
+    val pointerCount by viewModel.pointerCount.collectAsState()
+    val pointerName1 by viewModel.pointerName1.collectAsState()
+    val pointerName2 by viewModel.pointerName2.collectAsState()
+    val action by viewModel.pointerAction.collectAsState()
+    val activePointer by viewModel.activePointer.collectAsState()
 
     Surface(
         modifier = Modifier
             //.align(Alignment.Center)
-            .wrapContentSize()
-            .background(Color.Transparent),
+            .wrapContentSize(),
         shape = MaterialTheme.shapes.medium,
-        color = Color.Transparent,
+        color = Color.LightGray,
         tonalElevation = 0.dp,
         border = BorderStroke(1.dp, Color.DarkGray),
     ) {
@@ -108,12 +112,20 @@ fun BubbleContent(viewModel: BubbleViewModel) {
 
                 StylusVisualization(
                     modifier = Modifier
-                        .padding(top =  10.dp, start = 5.dp)
+                        .padding(top = 10.dp, start = 5.dp)
                         .height(30.dp)
                         .fillMaxWidth(),
                     viewModel = viewModel
 //                                .background(Color.Black.copy(alpha = 0.2f))
                 )
+
+                Row {
+                    Text(text = "nb:${pointerCount}", modifier = Modifier.padding(end = 5.dp))
+                    Text(text = "actPtr:${activePointer}", modifier = Modifier.padding(end = 5.dp))
+                    Text(text = "action:${action}", modifier = Modifier.padding(end = 5.dp))
+                    Text(text = "typ$pointerName1", modifier = Modifier.padding(end = 5.dp))
+                    Text(text = "typ$pointerName2")
+                }
                 HorizontalDivider(
                     thickness = 1.dp,
                     color = Color.Black
@@ -230,11 +242,62 @@ data class StylusState(
     var path: Path = Path(),
 )
 
-class DrawPoint(x: Float, y: Float, val type: DrawPointType) : PointF(x, y)
+class DrawPoint(x: Float, y: Float, val type: DrawPointType) : PointF(x, y){
+    fun copy(x: Float = this.x, y: Float = this.y, type: DrawPointType = this.type): DrawPoint = DrawPoint(x, y, type)
+}
 
 enum class DrawPointType {
     START,
     LINE
+}
+
+data class TwoFingersScrollState(
+    val startPoint: DrawPoint?,
+    val endPoint: DrawPoint?,
+) {
+    val xvar: Float?
+        get() = if (startPoint != null && endPoint != null)
+            endPoint.x - startPoint.x
+        else null
+    val yvar: Float?
+        get() = if (startPoint != null && endPoint != null)
+            endPoint.y - startPoint.y
+        else null
+    val isDefined: Boolean = startPoint != null && endPoint != null
+
+    companion object {
+        var instance: TwoFingersScrollState? = null
+
+        private fun ensureInstance(): TwoFingersScrollState {
+            if (instance == null)
+                instance = TwoFingersScrollState(null, null)
+            return instance!!
+        }
+
+        private fun defineInstance(state: TwoFingersScrollState) {
+            instance = state
+        }
+
+        fun setStartPoint(startPoint: DrawPoint): TwoFingersScrollState {
+            defineInstance(ensureInstance().copy(startPoint = startPoint))
+            return instance!!
+        }
+
+        fun setEndPoint(endPoint: DrawPoint): TwoFingersScrollState {
+            defineInstance(ensureInstance().copy(endPoint = endPoint))
+            return instance!!
+        }
+
+        val deltaX: Float?
+            get() = ensureInstance().xvar ?: 0f
+
+        val deltaY: Float?
+            get() = ensureInstance().yvar ?: 0f
+        
+        fun reset() {
+            instance = null
+        }
+    }
 }
 
 
--- app/src/main/java/lorry/folder/items/memogamma/bubble/BubbleViewModel.kt
+++ app/src/main/java/lorry/folder/items/memogamma/bubble/BubbleViewModel.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import lorry.folder.items.memogamma.bubble.BubbleManager.intentChannel
 import java.util.UUID
@@ -29,11 +30,29 @@ class BubbleViewModel @Inject constructor(
     private val _bubbleState = MutableStateFlow(BubbleState.BUBBLE)
     val bubbleState: StateFlow<BubbleState> = _bubbleState
 
+    private val _pointerCount = MutableStateFlow(0)
+    val pointerCount: StateFlow<Int> = _pointerCount
+
+    private val _activePointer = MutableStateFlow(0)
+    val activePointer: StateFlow<Int> = _activePointer
+
+    private val _pointerName1 = MutableStateFlow("")
+    val pointerName1: StateFlow<String> = _pointerName1
+
+    private val _pointerName2 = MutableStateFlow("")
+    val pointerName2: StateFlow<String> = _pointerName2
+
+    private val _action = MutableStateFlow("")
+    val pointerAction: StateFlow<String> = _action
+
+    private val _pointerAction2 = MutableStateFlow("")
+    val pointerAction2: StateFlow<String> = _pointerAction2
+
     private val _stylusColor = MutableStateFlow(Color.Black)
     val stylusColor: StateFlow<Color> = _stylusColor
-    
+
     var coroutineScope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-    
+
     private var _stylusState = MutableStateFlow(StylusState())
     val stylusState: StateFlow<StylusState> = _stylusState
 
@@ -58,35 +77,144 @@ class BubbleViewModel @Inject constructor(
     }
 
     fun processMotionEvent(motionEvent: MotionEvent): Boolean {
+        _pointerCount.update { motionEvent.pointerCount }
+        _activePointer.value = motionEvent.actionIndex;
+        _action.value = when (motionEvent.actionMasked) {
+            MotionEvent.ACTION_MOVE -> "ACTION_MOVE"
+            MotionEvent.ACTION_DOWN -> "ACTION_DOWN"
+            MotionEvent.ACTION_UP -> "ACTION_UP"
+            MotionEvent.ACTION_POINTER_DOWN -> "ACTION_POINTER_DOWN"
+            MotionEvent.ACTION_POINTER_UP -> "ACTION_POINTER_UP"
+            else -> "Inconnu"
+        }
+
+        if (motionEvent.pointerCount >= 1) {
+            _pointerName1.value = when (motionEvent.getToolType(0)) {
+                MotionEvent.TOOL_TYPE_STYLUS -> "0-Stylus"
+                MotionEvent.TOOL_TYPE_FINGER -> "0-Doigt"
+                MotionEvent.TOOL_TYPE_MOUSE -> "0-Souris"
+                MotionEvent.TOOL_TYPE_ERASER -> "0-Crayon"
+                else -> "0-Inconnu"
+            }
+        } else {
+            _pointerName1.value = ""
+            _pointerName2.value = ""
+        }
+        if (motionEvent.pointerCount >= 2)
+            _pointerName2.value = when (motionEvent.getToolType(1)) {
+                MotionEvent.TOOL_TYPE_STYLUS -> "1-Stylus"
+                MotionEvent.TOOL_TYPE_FINGER -> "1-Doigt"
+                MotionEvent.TOOL_TYPE_MOUSE -> "1-Souris"
+                MotionEvent.TOOL_TYPE_ERASER -> "1-Crayon"
+                else -> "1-Inconnu"
+            }
+        else {
+            _pointerName2.value = ""
+        }
+
         println("GAMMA: Event reçu : ${motionEvent.actionMasked}, x=${motionEvent.x}, y=${motionEvent.y}")
-        
-        if (motionEvent.getToolType(0) != MotionEvent.TOOL_TYPE_STYLUS)
-            return false
-        
+
         when (motionEvent.actionMasked) {
-            MotionEvent.ACTION_DOWN -> {
-                currentPath.add(
+            MotionEvent.ACTION_MOVE -> {
+                if (motionEvent.pointerCount >= 2 &&
+                    motionEvent.getToolType(0) == MotionEvent.TOOL_TYPE_FINGER
+                    && motionEvent.getToolType(1) == MotionEvent.TOOL_TYPE_FINGER
+                ) {
+                    println("GAMMA : Scroll à 2 doigts détecté")
+                    TwoFingersScrollState.setEndPoint(
+                        DrawPoint(motionEvent.x, motionEvent.y, DrawPointType.START)
+                    )
+
+                    currentPath = currentPath.map {
+                        it.copy(
+                            x = it.x + (TwoFingersScrollState.deltaX ?: 0f),
+                            y = it.y + (TwoFingersScrollState.deltaY ?: 0f)
+                        )
+                    }.toMutableList()
+
+                    requestRendering(
+                        StylusState(
+                            tilt = motionEvent.getAxisValue(MotionEvent.AXIS_TILT),
+                            pressure = motionEvent.pressure,
+                            orientation = motionEvent.orientation,
+                            path = createPath()
+                        )
+                    )
+
+                    TwoFingersScrollState.setStartPoint(
+                        DrawPoint(motionEvent.x, motionEvent.y, DrawPointType.START)
+                    )
+                }
+            }
+
+            MotionEvent.ACTION_POINTER_DOWN -> {
+                //2e pointeur abaissé
+                println("GAMMA pointer down")
+                TwoFingersScrollState.setStartPoint(
                     DrawPoint(motionEvent.x, motionEvent.y, DrawPointType.START)
                 )
             }
-            MotionEvent.ACTION_MOVE -> {
-                currentPath.add(DrawPoint(motionEvent.x, motionEvent.y, DrawPointType.LINE))
-            }
-            MotionEvent.ACTION_UP -> {
-                val canceled = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
-                        (motionEvent.flags and MotionEvent.FLAG_CANCELED) == MotionEvent.FLAG_CANCELED
 
-                if(canceled) {
-                    cancelLastStroke()
-                } else {
+            MotionEvent.ACTION_POINTER_UP -> {
+                //2e pointeur est levé
+                println("GAMMA pointer up")
+                TwoFingersScrollState.reset()
+            }
+
+            MotionEvent.ACTION_UP -> {
+                //1er pointeur est levé
+                println("GAMMA up")
+                _pointerCount.value = 0
+            }
+
+            MotionEvent.ACTION_CANCEL -> {
+                println("GAMMA cancel")
+                _pointerCount.value = 0
+            }
+        }
+
+        //main ou doigt
+        val stylusIndex = (0..motionEvent.pointerCount - 1).firstOrNull {
+            motionEvent.getToolType(it) == MotionEvent.TOOL_TYPE_STYLUS
+        } ?: -1
+
+        if (stylusIndex == -1)
+            return true
+        else
+        //stylet
+        {
+            if (motionEvent.actionIndex != stylusIndex)
+                return true
+
+            when (motionEvent.actionMasked) {
+                MotionEvent.ACTION_DOWN -> {
+                    currentPath.add(
+                        DrawPoint(motionEvent.x, motionEvent.y, DrawPointType.START)
+                    )
+                }
+
+                MotionEvent.ACTION_MOVE -> {
                     currentPath.add(DrawPoint(motionEvent.x, motionEvent.y, DrawPointType.LINE))
                 }
+
+                MotionEvent.ACTION_UP -> {
+                    val canceled = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+                            (motionEvent.flags and MotionEvent.FLAG_CANCELED) == MotionEvent.FLAG_CANCELED
+
+                    if (canceled) {
+                        cancelLastStroke()
+                    } else {
+                        currentPath.add(DrawPoint(motionEvent.x, motionEvent.y, DrawPointType.LINE))
+                    }
+                }
+
+                MotionEvent.ACTION_CANCEL -> {
+                    // Unwanted touch detected.
+                    cancelLastStroke()
+                }
+
+                else -> return true
             }
-            MotionEvent.ACTION_CANCEL -> {
-                // Unwanted touch detected.
-                cancelLastStroke()
-            }
-            else -> return false
         }
 
         requestRendering(
@@ -100,7 +228,7 @@ class BubbleViewModel @Inject constructor(
 
         return true
     }
-    
+
 
     fun setBubbleState(value: BubbleState) {
         _bubbleState.value = value
